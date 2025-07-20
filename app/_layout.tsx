c540d8fe90afd61e1ec0b66302c5a9774354e4e7
freda08 <178652360+freda08@users.noreply.github.com>
2025-07-20 13:12:00 +0300

    feat: layout for card

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,11 +2,13 @@ import { Stack } from "expo-router";
 
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import "@/global.css";
+import { Text } from "react-native";
 
 
 export default function RootLayout() {
     return (
         <GluestackUIProvider mode="light">
+            <Text>ddd</Text>
             <Stack />
         </GluestackUIProvider>
     );
--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,24 @@
 import { Box } from "@/components/ui/box";
+import { Button, ButtonIcon } from "@/components/ui/button";
+import {
+    Checkbox,
+    CheckboxIcon,
+    CheckboxIndicator,
+    CheckboxLabel,
+} from "@/components/ui/checkbox";
 import { Fab, FabIcon } from "@/components/ui/fab";
-import { AddIcon } from "@/components/ui/icon";
+import { HStack } from "@/components/ui/hstack";
+import { AddIcon, CheckIcon, CloseIcon, ThreeDotsIcon } from "@/components/ui/icon";
 import { Text } from "@/components/ui/text";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useRouter } from "expo-router";
 import { useEffect, useState } from "react";
 import { View } from "react-native";
 
-
 export default function Index() {
     const router = useRouter();
     const [data, setData] = useState([])
-  
+
     useEffect(() => {
         const loadData = async () => {
             try {
@@ -60,14 +67,55 @@ export default function Index() {
                 data.map(item => (
                     <Box
                         key={item.id}
-                        className="w-50 bg-gray-200 rounded-lg p-6 m-4 w-xl"
+                        className="w-50 bg-gray-200 rounded-lg p-6 mb-3 mt-3 mr-6 ml-4 w-xl"
+                        on={() => router.navigate('/add-todo')}
                     >
-                        <Text className="text-black-900">{item.name}</Text>
+                        <HStack space="md">
+                            <Box style={{ flex: 1 }}>
+                                <HStack space="md">
+                                    <Checkbox size="md" isInvalid={false} isDisabled={false}>
+                                        <CheckboxIndicator>
+                                            <CheckboxIcon as={CheckIcon} />
+                                        </CheckboxIndicator>
+                                        <CheckboxLabel>
+                                            <Box>
+                                                <Text className="text-black-900" size="xl">{item.name}</Text>
+                                                <Text className="text-black-900" size="xs">{item.date}</Text>
+                                            </Box>
+
+                                        </CheckboxLabel>
+                                    </Checkbox>
+
+
+                                </HStack>
+                            </Box>
+
+                            <HStack
+                                style={{
+                                    flex: 1,
+                                    alignItems: 'center',
+                                    justifyContent: 'flex-end'
+                                }}
+                                space="xl"
+
+                            >
+
+                                <Button size="xs" action="secondary" className="p-2">
+                                    <ButtonIcon as={ThreeDotsIcon} size="xl" />
+                                </Button>
+
+                                <Button size="xs" action="negative" className="p-2">
+                                    <ButtonIcon as={CloseIcon} size="xl" />
+                                </Button>
+
+                            </HStack>
+                        </HStack>
                     </Box>
                 ))
             }
 
             <Fab
+                className="mb-12 mr-6"
                 size="lg"
                 placement="bottom right"
                 isHovered={false}
